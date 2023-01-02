37158fa2d5c42a9c21a52d5b89b3b5bfe0ebd5ea
Joel De Conto <joel_d.c@hotmail.com>
2023-01-01 22:39:07 -0300

    .

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel}`);
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade máxima')
+}
+console.log('Dirija sempre com o cinto de segurança');
---
+++ javascript/aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA';
+if(pais == 'Brasil'){
+    console.log('Você é brasileiro');
+}else{
+    ('Você é estrangeiro!');
+}
